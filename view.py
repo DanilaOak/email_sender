d4663d8a5a49dcf2596c5f04e70f6b2fed934720
Danil Dubovyk <dubovyk.d@gmail.com>
2018-02-21 18:03:33 +0200

    add template

--- view.py
+++ view.py
@@ -1,4 +1,6 @@
 import json
+from jinja2 import Template
+import os
 
 from aiohttp import web
 
@@ -8,32 +10,48 @@ routes = web.RouteTableDef()
 
 import smtplib
 import email
+from email6.mime.multipart import MIMEMultipart
+from email6.mime.text import MIMEText
 
+
+base_dir = os.path.abspath(os.path.curdir)
 
 @routes.post('/api/v1/emails')
 @serialize_body('post_email')
 async def send_email(request: web.Request, body):
-    print(body)
     subject = None
 
     if 'subject' in body:
         subject = body['subject']
 
-    await email_sender(body['to_addr'], body['msg'], request.app['config'], subject)
+    message_template = await read_template(os.path.join(base_dir, 'email_template.html'))
+    # import ipdb; ipdb.set_trace()
+    # print(message_template.render())
+    # await email_sender(body['to_addr'], body['msg'], request.app['config'], subject)
+    await email_sender(body['to_addr'], message_template.render(), request.app['config'], subject)
     return web.Response(status=200, content_type='application/json', body=json.dumps({'status': 'Ok'}))
 
 
 async def email_sender(toaddr: str, body: str, config: dict, subject=None):
     fromaddr = config['EMAIL_ADDRESS']
-    msg = email.message.EmailMessage()
+
+
+    # msg = email.message.EmailMessage()
+    msg = MIMEMultipart('alternative')
     msg['From'] = fromaddr
     msg['To'] = toaddr
     msg['Subject'] = subject if subject else 'None'
 
-    msg.set_content(body)
+    msg.attach(MIMEText(body, 'html'))
 
     with smtplib.SMTP(config['SMTP_SERVER'], config['SMTP_PORT']) as server:
         server.starttls()
         server.login(fromaddr, config['EMAIL_PASSWORD'])
         text = msg.as_string()
         server.sendmail(fromaddr, toaddr, text)
+
+
+async def read_template(filename):
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template = Template(template_file.read())
+    return template
